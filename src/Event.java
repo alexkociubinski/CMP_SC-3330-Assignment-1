22f835185defad7bdb05913bf5bc68b883e57a3d
Alex Kociubinski <alexkociubinski@gmail.com>
2026-09-21 13:06:38 -0500

    Leave comments on event.java

--- src/Event.java
+++ src/Event.java
@@ -1,27 +1,34 @@
+//Create Event Class
 public final class Event {
 
+    // Create variables for name and location
     private final String name;
     private final String location;
 
+    // Create a new event
     public Event(String name, String location) {
-        if (location == null || location.isBlank()) {
+        if (location == null || location.isBlank()) { // Check for null or blank values
             throw new IllegalArgumentException("Event location can't be NULL!");
         }
-        if (name == null || name.isBlank()) {
+        if (name == null || name.isBlank()) { // Check for null or blank values
             throw new IllegalArgumentException("Event name can't be NULL!");
         }
+        // Save name and location
         this.name = name;
         this.location = location;
     }
 
+    // Get event name
     public String getName() {
         return name;
     }
 
+    // Get location name
     public String getLocation() {
         return location;
     }
 
+    // Return the event name as a string
     @Override
     public String toString() {
         return name + " at " + location;
